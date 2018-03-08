e796a80938129bfeb0b579c88b633a4ad570149a
Hovhannes Hambardzumyan <hovhannes@Shannons-Mac-mini.local>
2018-03-08 10:24:26 -0500

    Xpath works !

--- simple.js
+++ simple.js
@@ -8,16 +8,37 @@ driver.get('https://qashopscreen.microsignage.com/mgmt/index.html#!/login');
 driver.findElement(By.id('email_input')).sendKeys('qatelefonica@microsigns.com');
 driver.findElement(By.id('userPassword')).sendKeys('12345');
 driver.findElement(By.id('loginButton')).click();
+//driver.findElement(By.css('button[submit]')).click();
+
+/*driver.sleep(10000).then(function getElementByXpath(path) {
+    driver.findElement(By.xpath('//*[@id="loginButton"]')).click();
+    });*/
+    
+    driver.sleep(13000).then(function () {
+        driver.findElement(By.xpath('//*[@id="Content"]/div/button')).click();
+        });
+
+driver.sleep(14000).then(function() {
+        driver.findElement(By.id('CONTENT CATALOGS')).click();
+    });
+
+    
+
 //driver.findElement(By.id('Content')).click();
 
 
-driver.sleep(10000).then(function(){
-    var content = driver.findElement(By.id("Content")).then(function(){
+
+
+
+
+
+/*driver.sleep(10000).then(function(){
+    var content = driver.findElement(By.css("#Content")).then(function(){
         var child = content.firstChild;
         child.firstChild.click();
         
     });
-});
+});*/
 
 
 
