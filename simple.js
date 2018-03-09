afe00b5c92e7f2f90b9e9b18995a7ad72af23619
Hovhannes Hambardzumyan <hovhannes@Shannons-Mac-mini.local>
2018-03-09 11:14:02 -0500

    got text from td

--- simple.js
+++ simple.js
@@ -7,38 +7,45 @@ var driver = new webdriver.Builder().forBrowser('chrome').build();
 driver.get('https://qashopscreen.microsignage.com/mgmt/index.html#!/login');
 driver.findElement(By.id('email_input')).sendKeys('qatelefonica@microsigns.com');
 driver.findElement(By.id('userPassword')).sendKeys('12345');
-driver.findElement(By.id('loginButton')).click();
-//driver.findElement(By.css('button[submit]')).click();
 
-/*driver.sleep(10000).then(function getElementByXpath(path) {
-    driver.findElement(By.xpath('//*[@id="loginButton"]')).click();
-    });*/
+driver.sleep(10000).then(function () {
+        driver.findElement(By.id('loginButton')).click();
+});
+
     
-    driver.sleep(13000).then(function () {
+driver.sleep(15000).then(function () {
         driver.findElement(By.xpath('//*[@id="Content"]/div/button')).click();
         });
-
-driver.sleep(14000).then(function() {
+driver.sleep(15000).then(function() {
         driver.findElement(By.id('CONTENT CATALOGS')).click();
+        console.log("Success !!!")
     });
 
+
+
+    driver.sleep(17000).then(function() {
+
+    var table = driver.findElement(By.xpath('/html/body/div/div/div[2]/div[2]/content-catalogs/div/div[4]/table/tbody/tr[1]/td[4]')).getText();
+
+
+    driver.sleep(10000).then(function() {
+
+
+        console.log(table);
+
+    }); 
+}); 
     
 
-//driver.findElement(By.id('Content')).click();
-
-
-
-
-
-
-
-/*driver.sleep(10000).then(function(){
-    var content = driver.findElement(By.css("#Content")).then(function(){
-        var child = content.firstChild;
-        child.firstChild.click();
-        
+     /*
+    //Print data from each cell
+    for (var i=0; i < elements.length; i++){
+    elements[i].getText().then(function(txt){
+    console.log(txt + "\n");
     });
-});*/
+    }
+*/
+
 
 
 
